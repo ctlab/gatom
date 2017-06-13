2d42af71e4f4d3c5ae5a1e070fe31b5056553f1e
Alexey Sergushichev <alsergbox@gmail.com>
2017-06-14 00:22:52 +0300

    testing overall pipeline

--- tests/testthat/test_gatom.R
+++ tests/testthat/test_gatom.R
@@ -8,7 +8,7 @@ data("met.de.rawEx")
 data("gene.de.rawEx")
 
 
-test_that("gatom works", {
+test_that("overall pipeline works", {
     g <- makeAtomGraph(network=networkEx,
                        org.gatom.anno=org.Mm.eg.gatom.annoEx,
                        gene.de=gene.de.rawEx,
@@ -16,5 +16,16 @@ test_that("gatom works", {
                        met.de=met.de.rawEx)
     expect_is(g, "igraph")
     expect_true("Idh1" %in% E(g)$label)
+
+    gs <- scoreGraph(g, k.gene = 25, k.met=25)
+
+    expect_true(E(gs)[match("Idh1", label)]$score > 0)
+    expect_true(E(gs)[match("Gapdh", label)]$score < 0)
+
+
+    set.seed(42)
+    m <- solveSgmwcsRandHeur(gs, max.iterations = 2000)
+
+    expect_true("Idh1" %in% E(m)$label)
 })
 
