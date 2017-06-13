1eeec618966c1acfed90f65f41447af291c88522
Alexey Sergushichev <alsergbox@gmail.com>
2017-06-14 00:26:04 +0300

    failing test for working without met data

--- tests/testthat/test_gatom.R
+++ tests/testthat/test_gatom.R
@@ -29,3 +29,23 @@ test_that("overall pipeline works", {
     expect_true("Idh1" %in% E(m)$label)
 })
 
+test_that("overall pipeline works without met data", {
+    g <- makeAtomGraph(network=networkEx,
+                       org.gatom.anno=org.Mm.eg.gatom.annoEx,
+                       gene.de=gene.de.rawEx,
+                       met.db=met.kegg.dbEx,
+                       met.de=NULL)
+    expect_is(g, "igraph")
+    expect_true("Idh1" %in% E(g)$label)
+
+    gs <- scoreGraph(g, k.gene = 25)
+
+    expect_true(E(gs)[match("Idh1", label)]$score > 0)
+    expect_true(E(gs)[match("Gapdh", label)]$score < 0)
+
+
+    set.seed(42)
+    m <- solveSgmwcsRandHeur(gs, max.iterations = 2000)
+
+    expect_true("Idh1" %in% E(m)$label)
+})
