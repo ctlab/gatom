689ff4fff17992c9434e5613c2070645d4ae7617
Octopus <anastasiia.gainullina@gmail.com>
2017-11-14 19:47:19 +0300

    back to test_io

--- tests/testthat/test_io.R
+++ tests/testthat/test_io.R
@@ -12,28 +12,3 @@ test_that("saveModuleToDot works", {
     saveModuleToDot(mEx, f)
     expect_true(file.exists(f))
 })
-
-test_that("getDotNodeStyleAttributes and getDotEdgeStyleAttributes work", {
-    g <- makeAtomGraph(network=networkEx,
-                       org.gatom.anno=org.Mm.eg.gatom.annoEx,
-                       gene.de=gene.de.rawEx,
-                       met.db=met.kegg.dbEx,
-                       met.de=NULL)
-    gs <- scoreGraph(g, k.gene=25, k.met=NULL, show.warnings = FALSE)
-    set.seed(42)
-    m <- solveSgmwcsRandHeur(gs, max.iterations = 2000)
-    options(stringsAsFactors = FALSE)
-    produceNodeAttrs <- getDotNodeStyleAttributes(as_data_frame(m, what = "vertices"))
-
-
-    g <- makeAtomGraph(network=networkEx,
-                       org.gatom.anno=org.Mm.eg.gatom.annoEx,
-                       gene.de=NULL,
-                       met.db=met.kegg.dbEx,
-                       met.de=met.de.rawEx)
-    gs <- scoreGraph(g, k.gene=NULL, k.met=25, show.warnings = FALSE)
-    set.seed(42)
-    m <- solveSgmwcsRandHeur(gs, max.iterations = 2000)
-    options(stringsAsFactors = FALSE)
-    produceEdgeAttrs <- getDotEdgeStyleAttributes(as_data_frame(m))
-})
